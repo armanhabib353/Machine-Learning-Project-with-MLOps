ffccc5cfca04be0b514f82a8b1bff4bdcfda7124
armanhabib353 <armanhabib353@gmail.com>
2023-09-01 13:55:07 +0600

    new

--- src/Machine Learning Project with MLOps/utils/common.py
+++ src/Machine Learning Project with MLOps/utils/common.py
@@ -21,9 +21,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         ValueError: if yaml file is empty
         e: empty file
 
-    Returns:
-        ConfigBox: ConfigBox type
-    """
+    
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
