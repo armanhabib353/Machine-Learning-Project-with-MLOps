bc4bdfd0ab139a8938fff477c4dbf9af6706ab6c
armanhabib353 <armanhabib353@gmail.com>
2023-09-03 15:16:43 +0600

    new

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
-   
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
