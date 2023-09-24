394d1ad05f9d603808da1cb86c56ad69b67ef2d6
armanhabib353 <armanhabib353@gmail.com>
2023-09-24 14:29:16 +0600

    new

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
